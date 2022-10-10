fb4e3d853dabb23af07092eccd8ac2752ea2a111
sadmankiba <sadmankiba@roman.cs.wisc.edu>
2022-10-09 22:24:07 -0500

    shell: read from file

--- processes_shell/wish.c
+++ processes_shell/wish.c
@@ -2,14 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main () {
+int main (int argc, char* argv[]) {
+    int batch = 0;
+    FILE *fptr;
+    if(argc > 1) {
+        batch = 1;
+        fptr = fopen(argv[1], "r");
+    }
     while(1) {
-        printf("wish> ");
+        if (batch == 0)
+            printf("wish> ");
         size_t bln = 100;
         char *bin = (char *) malloc(bln * sizeof(char));
-
-        int chrs = getline(&bin, &bln, stdin);
-        bin[chrs-1]='\0';
+        if(batch == 1) {
+            fgets(bin, bln, fptr);
+        } else {
+            getline(&bin, &bln, stdin);
+        }
+        bin[strlen(bin)-1]='\0';
         if(strcmp(bin, "exit") == 0) {
             exit(0);
         } else {
