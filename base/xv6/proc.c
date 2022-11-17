32717dee04bf8e00085b34f4f90407dcc93595fe
sadmankiba <sadmankiba@cs.wisc.edu>
2022-11-16 18:26:46 -0600

    Comment out join

--- base/xv6/proc.c
+++ base/xv6/proc.c
@@ -276,43 +276,44 @@ exit(void)
 int 
 join(void)
 {
-  struct proc *p;
-  int havesibs, pid;
-  struct proc *curproc = myproc();
+  // struct proc *p;
+  // int havesibs, pid;
+  // struct proc *curproc = myproc();
   
-  acquire(&ptable.lock);
-  for(;;){
-    // Scan through table looking for sibling thread.
-    havesibs = 0;
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->sibling != curproc) // Correct?
-        continue;
-      havesibs = 1;
-      if(p->state == ZOMBIE){
-        // Found one.
-        pid = p->pid;
-        kfree(p->kstack);
-        p->kstack = 0;
-        freevm(p->pgdir);
-        p->pid = 0;
-        p->sibling = 0;
-        p->name[0] = 0;
-        p->killed = 0;
-        p->state = UNUSED;
-        release(&ptable.lock);
-        return pid;
-      }
-    }
+  // acquire(&ptable.lock);
+  // for(;;){
+  //   // Scan through table looking for sibling thread.
+  //   havesibs = 0;
+  //   for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+  //     if(p->sibling != curproc) // Correct?
+  //       continue;
+  //     havesibs = 1;
+  //     if(p->state == ZOMBIE){
+  //       // Found one.
+  //       pid = p->pid;
+  //       kfree(p->kstack);
+  //       p->kstack = 0;
+  //       freevm(p->pgdir);
+  //       p->pid = 0;
+  //       p->sibling = 0;
+  //       p->name[0] = 0;
+  //       p->killed = 0;
+  //       p->state = UNUSED;
+  //       release(&ptable.lock);
+  //       return pid;
+  //     }
+  //   }
 
-    // No point waiting if we don't have any children.
-    if(!havesibs || curproc->killed){
-      release(&ptable.lock);
-      return -1;
-    }
+  //   // No point waiting if we don't have any children.
+  //   if(!havesibs || curproc->killed){
+  //     release(&ptable.lock);
+  //     return -1;
+  //   }
 
-    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
-    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
-  }
+  //   // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+  //   sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  // }
+  return 0;
 }
 
 // Wait for a child process to exit and return its pid.
