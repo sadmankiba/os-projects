b24f74c4d08b035405d8f1a84e1cd5463d830241
Sadman Sakib <sadmankiba@cs.wisc.edu>
2023-06-22 02:56:33 +0000

    Correct locking example

--- concepts/locking/example/locking.c
+++ concepts/locking/example/locking.c
@@ -2,6 +2,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/spinlock.h>
+#include <linux/kthread.h>
 
 MODULE_DESCRIPTION("Locking example module");
 MODULE_AUTHOR("Sadman Sakib");
@@ -9,30 +10,29 @@ MODULE_LICENSE("GPL");
 
 static DEFINE_SPINLOCK(my_lock);
 static int count;
+struct task_struct *thread1, *thread2;
 
-void incr(void)
+int incr(void *arg)
 {
+    int i;
     spin_lock(&my_lock);
     /* critical section */
-    for (int i = 0; i < 1000000; i++)
+    for (i = 0; i < 1000000; i++)
         count++;
     spin_unlock(&my_lock);
+    return 0;
 }
 
 static int locking_init(void)
 {
 	pr_debug("Hello!\n");
-    // Create two threads that increment count
-    struct task_struct *thread1, *thread2;
+    
     thread1 = kthread_create(incr, NULL, "thread1");
     thread2 = kthread_create(incr, NULL, "thread2");
     
     wake_up_process(thread1);
     wake_up_process(thread2);
-
-    // write code 
-    // wait for threads to finish
-	return 0;
+    return 0;
 }
 
 static void locking_exit(void)
