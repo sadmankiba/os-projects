bde6971e3a50be75dc2876d2cfe54f0dcee3bdfa
sadmankiba <sadmankiba@cs.wisc.edu>
2023-06-27 18:29:05 -0500

    Add parent, next in proc kernel exmp

--- concepts/proc-kernel/proc-kernel.c
+++ concepts/proc-kernel/proc-kernel.c
@@ -2,7 +2,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>        /* task_struct */
-#include <linux/sched/signal.h> /* for_each_process*/
+#include <linux/sched/signal.h> /* for_each_process */
 
 
 MODULE_DESCRIPTION("Kernel process");
@@ -11,9 +11,14 @@ MODULE_LICENSE("GPL");
 
 static int proc_kernel_init(void)
 {
-	struct task_struct *p;
+	struct task_struct *p, *nxt;
 
-    pr_info("Current process: pid = %d, cmd = %s", current->pid, current->comm);
+    pr_info("Current process: pid = %u, cmd = %s, utime = %llu", current->pid, current->comm, current->utime);
+    pr_info("Parent process: pid = %u, cmd = %s, utime = %llu", current->parent->pid, current->parent->comm, current->parent->utime);
+    
+    nxt = next_task(current);
+    if (nxt)
+        pr_info("Next process: pid = %u, cmd = %s, utime = %llu", nxt->pid, nxt->comm, nxt->utime);
 
     pr_info("Process list");
     for_each_process(p) {
