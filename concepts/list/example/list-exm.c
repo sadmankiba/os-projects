abfaeaa1fafcb71085d009a311ec3fdab319fbe3
sadmankiba <sadmankiba@cs.wisc.edu>
2023-06-28 11:06:08 -0500

    Add rwlock in list-exm

--- concepts/list/example/list-exm.c
+++ concepts/list/example/list-exm.c
@@ -8,7 +8,7 @@ MODULE_DESCRIPTION("List example");
 MODULE_AUTHOR("Kernel Practitioner");
 MODULE_LICENSE("GPL");
 
-/* Example of list and atomic */
+/* Example of list, atomic and rwlock */
 
 struct port_rec {
     struct list_head list;
@@ -27,58 +27,77 @@ LIST_HEAD(my_list);
 */
 LIST_HEAD(my_list); 
 
+DEFINE_RWLOCK(lock);
+
 int add_port(int num) {
     struct port_rec *pr;
     if((pr = search_port(num)) != NULL) {
+        write_lock(&lock);
         atomic_inc(&pr->count);
+        write_unlock(&lock);
         return 0;
     }
     pr = kzalloc(sizeof(struct port_rec), GFP_KERNEL);
     pr->num = num;
     pr->timestamp = jiffies;
     atomic_set(&pr->count, 1);
+    write_lock(&lock);
     list_add(&pr->list, &my_list);
+    write_unlock(&lock);
     return 0;
 }
 
 int delete_port(int num) {
     struct list_head *temp;
+    write_lock(&lock);
     list_for_each(temp, &my_list) {
         struct port_rec *pr = list_entry(temp, struct port_rec, list);
         if (pr->num == num) {
             list_del(temp);
             kfree(pr);
+            write_unlock(&lock);
             return 0;
         }
     }
+    write_unlock(&lock);
     return -EINVAL; 
 }
 
 void delete_all(void) {
     struct list_head *p, *q;
     struct port_rec *pr;
+
+    write_lock(&lock);
     list_for_each_safe(p, q, &my_list) {
         pr = list_entry(p, struct port_rec, list);
         list_del(p);
         kfree(pr);
     }
+    write_unlock(&lock);
 }
 
 struct port_rec * search_port(num) {
     struct port_rec *pr;
+    read_lock(&lock);
     list_for_each_entry(pr, &my_list, list) {
-        if(pr->num == num)
+        if(pr->num == num) {
+            read_unlock(&lock);
             return pr;
+        }
     }
+    read_unlock(&lock);
     return NULL;
 }
 
 void print_list(void) {
     struct port_rec *pr;
     pr_debug("List: ");
+    
+    read_lock(&lock);
     list_for_each_entry(pr, &my_list, list) {
         pr_debug("num = %d, timestamp = %lu, count = %d", pr->num, pr->timestamp, atomic_read(&pr->count));
     }
+    read_unlock(&lock);
 }
 
 static int listex_init(void)
