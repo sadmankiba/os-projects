75551e9a88c4f5c7533ba11b44ae28c653a71f53
sadmankiba <sadmankiba@cs.wisc.edu>
2023-07-21 10:07:48 -0500

    Complete work example

--- concepts/9-tasks/work/work.c
+++ concepts/9-tasks/work/work.c
@@ -1,7 +1,8 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
-// #include <linux/workqueue.h>
+#include <linux/workqueue.h> /* work, workqueue */
+#include <linux/delay.h> /* msleep */
 
 MODULE_DESCRIPTION("work module");
 MODULE_AUTHOR("Kernel Practitioner");
@@ -9,28 +10,51 @@ MODULE_LICENSE("GPL");
 
 void my_work_handler(struct work_struct *work);
 
-// struct work_struct my_work;
-// INIT_WORK(&my_work, my_work_handler);
-
 DECLARE_WORK(my_work, my_work_handler);
+
+struct workqueue_struct *my_wq;
+bool use_queue;
+
+/* 
+ * A work is only executed once whether it is scheduled or queued.
+ * If it is scheduled again, it is executed again. 
+ */
 
 static int work_init(void)
 {
 	pr_debug("work init!");
-	schedule_work(&my_work);
+	use_queue = true;
+	if (use_queue) {
+		my_wq = create_workqueue("my_wq");
+		queue_work(my_wq, &my_work);
+	} else 
+		schedule_work(&my_work);
 	return 0;
 }
 
 static void work_exit(void)
 {
-	flush_scheduled_work();
-	cancel_work_sync(&my_work);
+	if(use_queue) {
+		destroy_workqueue(my_wq);
+		flush_workqueue(my_wq);
+	} else {
+		cancel_work_sync(&my_work);
+		flush_scheduled_work();
+	}
+
 	pr_debug("work exit!");
 }
 
 void my_work_handler(struct work_struct *work)
 {
-	pr_debug("my_work_handler.");
+	pr_debug("my_work_handler enter.");
+	msleep(500);
+	if(use_queue) {
+		queue_work(my_wq, &my_work);
+	} else {
+		schedule_work(&my_work);
+	}
+	pr_debug("my_work_handler exit.");
 }
 
 module_init(work_init);
